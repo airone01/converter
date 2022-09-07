8efd9e3db2d4a9dca23b3f4ee5db3e4f8f5a7f09
AirOne01 <popgthyrd@gmail.com>
2022-09-07 15:56:43 +0200

    Revert "fix: remove useless ENV variable"
    This reverts commit 4e073a2e7cb45d467ff885d1fe9b2a51af95f5df.

--- web/install-submodules.sh
+++ web/install-submodules.sh
@@ -28,8 +28,8 @@ cd tmp # go into the tmp folder
 # checkout the current submodule commit
 git init # initialise empty repo
 git remote add origin "https://$GITHUB_ACCESS_TOKEN@$SUBMODULE_GITHUB" # add origin of the submodule
-git fetch --depth=1 origin main # fetch only the required version
-git checkout main # checkout on the right commit
+git fetch --depth=1 origin "$COMMIT" # fetch only the required version
+git checkout "$COMMIT" # checkout on the right commit
 
 # move the submodule from tmp to the submodule path
 cd .. # go folder up
