47ab9169d961cad33be580ed752b2b059cfaa909
AirOne01 <popgthyrd@gmail.com>
2022-10-11 00:32:29 +0200

    Add some SVGs

--- web/src/components/UploadButton.tsx
+++ web/src/components/UploadButton.tsx
@@ -19,13 +19,23 @@ export default function UploadButton({ atom: fileAtom }: Props) {
   
 
   return (
-    <div {...getRootProps()}>
+    <button {...getRootProps()} className="uploadButton">
       <input {...getInputProps()} />
       {
         isDragActive ?
-          <p>drop here</p> :
-          <p>upload</p>
+          <>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 019 9v.375M10.125 2.25A3.375 3.375 0 0113.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 013.375 3.375M9 15l2.25 2.25L15 12" />
+            </svg>
+            <p>drop here</p>
+          </> :
+          <>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m6.75 12l-3-3m0 0l-3 3m3-3v6m-1.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+            </svg>
+            <p>upload</p>
+          </>
       }
-    </div>
+    </button>
   )
 }
